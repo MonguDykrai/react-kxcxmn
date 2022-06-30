ec7a288120b674959ce4fcf7b21c48925b038bf1
MonguDykrai <kisaihou@gmail.com>
2022-06-30 17:25:04 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-import { Button, PlusOutlined } from 'antd';
+import { Button, PlusOutlined, Switch, Table } from 'antd';
 import 'antd/dist/antd.css';
 import FieldSelect from './FieldSelect';
 import _ from 'lodash';
 
 export default function App() {
+  const [checked, setChecked] = useState(false);
   const dataSource = [
     {
       id: 0,
@@ -46,6 +47,9 @@ export default function App() {
           />
         );
       })}
+      <div>
+        <Switch defaultChecked onChange={(checked) => console.log(checked)} />
+      </div>
       {/* <FieldSelect fields={fields} /> */}
       <Button
         onClick={() => {
@@ -62,6 +66,28 @@ export default function App() {
         +
       </Button>
       {/* <PlusOutlined /> */}
+      <Table
+        columns={[
+          {
+            title: '开启效果流',
+            dataIndex: 'checked',
+            key: 'checked',
+            render: (value, record) => (
+              <Switch
+                key={record.id}
+                defaultChecked
+                onChange={(checked) => console.log(checked)}
+              />
+            ),
+          },
+        ]}
+        dataSource={[
+          {
+            id: 0,
+            checked: true,
+          },
+        ]}
+      />
     </div>
   );
 }
